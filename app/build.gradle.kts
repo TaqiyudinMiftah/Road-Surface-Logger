8f1779fc083dfaff4d1b35ec2d83f0d98c0e1a9f
Taqiyudin Miftah Adn <143085259+TaqiyudinMiftah@users.noreply.github.com>
2026-08-11 11:46:00 +0700

    Bump app version to 0.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.roadsurfacelogger"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.2.1"
     }
 
     buildTypes {
